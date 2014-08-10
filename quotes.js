6d780eb87f7891192c5b9bb91086956e8ae7f899
Jessica Lord <jessica.lord@github.com>
2014-08-09 17:59:01 -0700

    awesome and amazing

---
+++ quotes.js
@@ -0,0 +1,15 @@
+var data =
+[
+  {
+    "source": "Jessica Lord",
+    "quote": "Today I made some lentil soup."
+  },
+  {
+    "source": "Jessica Lord",
+    "quote": "I went to Joaquin Miller park."
+  },
+  {
+    "source": "Jessica Lord",
+    "quote": "It is Saturday!"
+  }
+]
